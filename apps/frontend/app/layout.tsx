2c049b9fa7fa81a6485b6ddba0051ee6a6c5700a
Manuel De Toma <manuel.detoma@gmail.com>
2025-08-18 10:30:35 +0200

    feat: add shadcn

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${redditMono.className} antialiased`}
+        className={`${redditMono.className} antialiased dark`}
       >
         <Providers>
           {children}
