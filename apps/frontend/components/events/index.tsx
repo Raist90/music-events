bfb5e8d820829e8467f44ecc954ae5061622a555
Manuel De Toma <manuel.detoma@gmail.com>
2025-08-19 22:30:33 +0200

    feat: invert text and image positions

--- apps/frontend/components/events/index.tsx
+++ apps/frontend/components/events/index.tsx
@@ -21,15 +21,15 @@ export default function Events() {
       {data?._embedded.events.length && (
         <ul className="grid lg:grid-cols-4 gap-12">{data._embedded.events.map((event) => (
           <li className="flex flex-col gap-y-4 justify-between" key={event.id}>
+            <div className="aspect-video w-full">
+              <img className="object-fill size-full rounded-md" src={event.images.find(({ ratio, width }) => ratio === "16_9" && width > 1000)?.url} alt={event.name} />
+            </div>
+
             <a className="group" href={event.url} target="_blank">
               <h2 className="font-bold group-hover:underline">{event.name}</h2>
               <p className="text-sm mt-1">{event._embedded.venues[0].name}</p>
               <p className="text-xs mt-1">{format(event.dates.start.dateTime)}</p>
             </a>
-
-            <div className="aspect-video w-full">
-              <img className="object-fill size-full rounded-md" src={event.images.find(({ ratio, width }) => ratio === "16_9" && width > 1000)?.url} alt={event.name} />
-            </div>
           </li>
         ))}
         </ul>
