1fb57f9935b86b7868d0847f815d3a423c0df64a
Manuel De Toma <manuel.detoma@gmail.com>
2026-04-26 15:43:33 +0200

    feat(FE): add auth middleware

--- apps/frontend/lib/client/client.ts
+++ apps/frontend/lib/client/client.ts
@@ -1,6 +1,46 @@
-import createClient from "openapi-fetch";
+import createClient, { Middleware } from "openapi-fetch";
 import type { paths } from "@/schema";
+
+let accessToken: string | undefined = undefined;
+
+const UNPROTECTED_PATHS: (keyof paths)[] = [
+  "/auth/login",
+  "/auth/logout",
+  "/auth/refresh",
+  "/auth/register",
+  "/events",
+];
+
+const authMiddleware: Middleware = {
+  async onRequest({ request, schemaPath }) {
+    if (UNPROTECTED_PATHS.some((path) => schemaPath.startsWith(path))) {
+      return undefined;
+    }
+
+    if (!accessToken) {
+      try {
+        const { data, error, response } = await apiClient.POST(
+          "/auth/refresh",
+          {
+            credentials: "include",
+          },
+        );
+        if (error || !response.ok) {
+          throw new Error("Failed to refresh access token: " + response.status);
+        }
+        accessToken = data?.access_token;
+      } catch (err) {
+        const message =
+          err instanceof Error ? err.message : "Unexpected exception";
+        console.error("Error refreshing access token: " + message);
+      }
+      request.headers.set("Authorization", `Bearer ${accessToken}`);
+      return request;
+    }
+  },
+};
 
 export const apiClient = createClient<paths>({
   baseUrl: process.env.NEXT_PUBLIC_API_URL,
 });
+apiClient.use(authMiddleware);
